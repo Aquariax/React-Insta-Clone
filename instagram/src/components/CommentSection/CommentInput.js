67bdfa0824820cb5cdf0dd4728e7d042b6d90e66
Aquariax <roberthrrs8@gmail.com>
2019-04-09 19:27:52 -0400

    Day 2 mvp done now

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
- const CommentInput = props => {
+const CommentInput = props => {
   return (
-    <form>
-      <input type="text" placeholder="Add comment... " />
+    <form onSubmit={props.submitComment}>
+      <input
+        type="text"
+        value={props.comment}
+        placeholder="Add comment... "
+        onChange={props.changeComment}
+      />
     </form>
   );
 };
 
- export default CommentInput; 
+export default CommentInput;
